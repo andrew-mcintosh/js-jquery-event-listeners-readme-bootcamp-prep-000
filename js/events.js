96472f722bc4c1de3780f0cd09b4ae24d26f5ecb
Andrew McIntosh <andrewdavidmcintosh@gmail.com>
2018-02-02 19:31:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-$(document).ready(function(){
-   $("p").click(function getIt(){
+function getIt(){
+  $("p").click(function(){
       alert ('Hey!');
     });
-});
+}
 
-$('img').addClass("tasty");
+function frameIt(){
+   $('img').addClass("tasty");
+}
+
+function pressIt(){
+  $('form').keydown(function(key){
+          if (key.which == 71){
+           alert('G was pressed');
+            }
+          });
+}
+
+function submitIt(){
+  $('form').submit(function(){
+      alert('your form is going to be submitted now');
+               });
+}
+
+   $(document).ready(function(){
+         
+          getIt();
+          frameIt();
+          pressIt();
+          submitIt();
+        
+});
